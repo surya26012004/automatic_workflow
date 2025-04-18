d2bb27a2ffaa7af87a5764170952ccb9d03ec72e
surya <surya9393.k@gmail.com>
2025-04-18 23:07:16 +0530

    testing workflow

--- src/app/(main)/(pages)/workflows/_actions/workflow-connections.tsx
+++ src/app/(main)/(pages)/workflows/_actions/workflow-connections.tsx
@@ -1,179 +1,180 @@
 'use server'
 import { Option } from '@/components/ui/multiple-selector'
 import { db } from '@/lib/db'
-
-interface Channel {
-    value: string;
-    label: string;
-}
+import { auth, currentUser } from '@clerk/nextjs'
 
 export const getGoogleListener = async () => {
-    const userId = "9393"
+  const { userId } = auth()
 
-    if (userId) {
-        const listener = await db.user.findUnique({
-            where: {
-                clerkId: userId,
-            },
-            select: {
-                googleResourceId: true,
-            },
-        })
+  if (userId) {
+    const listener = await db.user.findUnique({
+      where: {
+        clerkId: userId,
+      },
+      select: {
+        googleResourceId: true,
+      },
+    })
 
-        if (listener) return listener
-    }
+    if (listener) return listener
+  }
 }
 
 export const onFlowPublish = async (workflowId: string, state: boolean) => {
-    console.log(state)
-    const published = await db.workflows.update({
-        where: {
-            id: workflowId,
-        },
-        data: {
-            publish: state,
-        },
-    })
+  console.log(state)
+  const published = await db.workflows.update({
+    where: {
+      id: workflowId,
+    },
+    data: {
+      publish: state,
+    },
+  })
 
-    if (published.publish) return 'Workflow published'
-    return 'Workflow unpublished'
+  if (published.publish) return 'Workflow published'
+  return 'Workflow unpublished'
 }
 
 export const onCreateNodeTemplate = async (
-    content: string,
-    type: string,
-    workflowId: string,
-    channels?: Channel[],
-    accessToken?: string,
-    notionDbId?: string
+  content: string,
+  type: string,
+  workflowId: string,
+  channels?: Option[],
+  accessToken?: string,
+  notionDbId?: string
 ) => {
-    if (type === 'Discord') {
-        const response = await db.workflows.update({
-            where: {
-                id: workflowId,
-            },
-            data: {
-                discordTemplate: content,
-            },
-        })
+  if (type === 'Discord') {
+    const response = await db.workflows.update({
+      where: {
+        id: workflowId,
+      },
+      data: {
+        discordTemplate: content,
+      },
+    })
 
-        if (response) {
-            return 'Discord template saved'
-        }
+    if (response) {
+      return 'Discord template saved'
     }
-    if (type === 'Slack') {
-        const response = await db.workflows.update({
-            where: {
-                id: workflowId,
-            },
-            data: {
-                slackTemplate: content,
-                slackAccessToken: accessToken,
-            },
-        })
+  }
+  if (type === 'Slack') {
+    const response = await db.workflows.update({
+      where: {
+        id: workflowId,
+      },
+      data: {
+        slackTemplate: content,
+        slackAccessToken: accessToken,
+      },
+    })
 
-        if (response) {
-            const channelList = await db.workflows.findUnique({
-                where: {
-                    id: workflowId,
+    if (response) {
+      const channelList = await db.workflows.findUnique({
+        where: {
+          id: workflowId,
+        },
+        select: {
+          slackChannels: true,
+        },
+      })
+
+      if (channelList) {
+        //remove duplicates before insert
+        const NonDuplicated = channelList.slackChannels.filter(
+          (channel) => channel !== channels![0].value
+        )
+
+        NonDuplicated!
+          .map((channel) => channel)
+          .forEach(async (channel) => {
+            await db.workflows.update({
+              where: {
+                id: workflowId,
+              },
+              data: {
+                slackChannels: {
+                  push: channel,
                 },
-                select: {
-                    slackChannels: true,
-                },
+              },
             })
+          })
 
-            if (channelList) {
-                //remove duplicates before insert
-                const NonDuplicated = channelList.slackChannels.filter(
-                    (channel: string) => channel !== channels![0].value
-                )
-
-                NonDuplicated!
-                    .map((channel: string) => channel)
-                    .forEach(async (channel: string) => {
-                        await db.workflows.update({
-                            where: {
-                                id: workflowId,
-                            },
-                            data: {
-                                slackChannels: {
-                                    push: channel,
-                                },
-                            },
-                        })
-                    })
-
-                return 'Slack template saved'
-            }
-            channels!
-                .map((channel) => channel.value)
-                .forEach(async (channel) => {
-                    await db.workflows.update({
-                        where: {
-                            id: workflowId,
-                        },
-                        data: {
-                            slackChannels: {
-                                push: channel,
-                            },
-                        },
-                    })
-                })
-            return 'Slack template saved'
-        }
-    }
-
-    if (type === 'Notion') {
-        const response = await db.workflows.update({
+        return 'Slack template saved'
+      }
+      channels!
+        .map((channel) => channel.value)
+        .forEach(async (channel) => {
+          await db.workflows.update({
             where: {
-                id: workflowId,
+              id: workflowId,
             },
             data: {
-                notionTemplate: content,
-                notionAccessToken: accessToken,
-                notionDbId: notionDbId,
+              slackChannels: {
+                push: channel,
+              },
             },
+          })
         })
-
-        if (response) return 'Notion template saved'
+      return 'Slack template saved'
     }
+  }
+
+  if (type === 'Notion') {
+    const response = await db.workflows.update({
+      where: {
+        id: workflowId,
+      },
+      data: {
+        notionTemplate: content,
+        notionAccessToken: accessToken,
+        notionDbId: notionDbId,
+      },
+    })
+
+    if (response) return 'Notion template saved'
+  }
 }
 
 export const onGetWorkflows = async () => {
-    const userId = "9393"
+  const user = await currentUser()
+  if (user) {
     const workflow = await db.workflows.findMany({
-        where: {
-            userId: userId,
-        },
+      where: {
+        userId: user.id,
+      },
     })
 
     if (workflow) return workflow
+  }
 }
 
 export const onCreateWorkflow = async (name: string, description: string) => {
-    const userId = "9393"
+  const user = await currentUser()
+
+  if (user) {
     //create new workflow
     const workflow = await db.workflows.create({
-        data: {
-            userId: userId,
-            name,
-            description,
-        },
+      data: {
+        userId: user.id,
+        name,
+        description,
+      },
     })
 
     if (workflow) return { message: 'workflow created' }
     return { message: 'Oops! try again' }
+  }
 }
 
 export const onGetNodesEdges = async (flowId: string) => {
-    const nodesEdges = await db.workflows.findUnique({
-        where: {
-            id: flowId,
-        },
-        select: {
-            nodes: true,
-            edges: true,
-        },
-    })
-    if (nodesEdges?.nodes && nodesEdges?.edges) return nodesEdges
+  const nodesEdges = await db.workflows.findUnique({
+    where: {
+      id: flowId,
+    },
+    select: {
+      nodes: true,
+      edges: true,
+    },
+  })
+  if (nodesEdges?.nodes && nodesEdges?.edges) return nodesEdges
 }
